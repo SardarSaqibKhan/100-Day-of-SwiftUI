78bfca43d004efc8d0b56b7c3d8119dd247d9357
Sardar Saqib <44336686+SardarSaqibKhan@users.noreply.github.com>
2024-12-03 13:13:17 +0500

    added project 1

--- 01-Project1/01-Project1/ContentView.swift
+++ 01-Project1/01-Project1/ContentView.swift
@@ -8,14 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = ""
+    @State private var tipPercentage = 0
+    @FocusState private var amountIsFocused: Bool
+    let tipPercentages = [0, 5, 10, 15, 20, 25]
+    
+    var grandTotal: Double {
+            let tipSelection = Double(tipPercentages[tipPercentage])
+            let orderAmount = Double(checkAmount) ?? 0
+            let tipValue = orderAmount / 100 * tipSelection
+            let grandTotal = orderAmount + tipValue
+            return grandTotal
         }
-        .padding()
+        
+        var totalPerPerson: Double {
+            let peopleCount = Double(numberOfPeople) ?? 1
+            let amountPerPerson = grandTotal / peopleCount
+            return amountPerPerson
+        }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    TextField("Number of people", text: $numberOfPeople)
+                        .keyboardType(.numberPad)
+                        .focused($amountIsFocused)
+                }
+                
+                Section("How much tip do you want to leave?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                .textCase(nil)
+                
+                Section {
+                    Text("\(grandTotal, specifier: "%.2f")")
+                } header: {
+                    Text("Total Amounnt:")
+                }
+                .textCase(nil)
+                
+                Section {
+                    Text("\(totalPerPerson, specifier: "%.2f")")
+                } header: {
+                    Text("Amounnt Per Person:")
+                }
+                .textCase(nil)
+            }
+            .navigationTitle("Wesplit SwiftUI Project-1")
+            .navigationBarTitleDisplayMode(.inline)
+            
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 

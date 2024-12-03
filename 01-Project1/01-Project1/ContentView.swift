ffdde6a718e6905cb608162a7f55f31ad1034aa5
Sardar Saqib <44336686+SardarSaqibKhan@users.noreply.github.com>
2024-12-03 11:03:47 +0500

    created project 1

---
+++ 01-Project1/01-Project1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  01-Project1
+//
+//  Created by sardar saqib on 03/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01-Project1/01-Project1/_1_Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  _1_Project1App.swift
+//  01-Project1
+//
+//  Created by sardar saqib on 03/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct _1_Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

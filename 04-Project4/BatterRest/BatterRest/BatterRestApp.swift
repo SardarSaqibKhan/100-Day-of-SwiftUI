f9dec3abfdcddd678a2e86a8a7c43cfbaa11fee4
Sardar Saqib <44336686+SardarSaqibKhan@users.noreply.github.com>
2024-12-18 12:10:07 +0500

    project 4 sleep time prediction on bases of cofee and other use cases

---
+++ 04-Project4/BatterRest/BatterRest/BatterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatterRestApp.swift
+//  BatterRest
+//
+//  Created by sardar saqib on 18/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct BatterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

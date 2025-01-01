5733f2809894645d1d6b0521e882889fe5eb9b40
Sardar Saqib <44336686+SardarSaqibKhan@users.noreply.github.com>
2025-01-01 11:13:14 +0500

    project 11 setup created

---
+++ 11-Project11/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by sardar saqib on 01/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

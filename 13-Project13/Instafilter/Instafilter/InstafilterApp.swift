6026079d9099faff3f2e0d4c6e0fc2b45ece6752
Sardar Saqib <44336686+SardarSaqibKhan@users.noreply.github.com>
2025-01-03 11:04:47 +0500

    added project 13 structure

---
+++ 13-Project13/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by sardar saqib on 03/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
